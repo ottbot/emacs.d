9f6299283c98f265f45014df5d5761e1e24f76e1
Robert Crim <rob@servermilk.com>
2014-06-13 14:41:35 -0700

    Updates to clojure conf

--- clojure-conf.el
+++ clojure-conf.el
@@ -1,3 +1,17 @@
+(require 'clojure-mode)
+
 (add-hook 'clojure-mode-hook 'paredit-mode)
+(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
+
+(define-clojure-indent
+  (defroutes 'defun)
+  (GET 2)
+  (POST 2)
+  (PUT 2)
+  (DELETE 2)
+  (HEAD 2)
+  (ANY 2)
+  (context 2))
 
 (provide 'clojure-conf)
