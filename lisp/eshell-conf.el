4a8e6a1be26a57a70e852c794e0479c0a6a9e015
Robert Crim <rob@servermilk.com>
2015-03-04 09:47:50 -0800

    Add some eshell funs

--- lisp/eshell-conf.el
+++ lisp/eshell-conf.el
@@ -8,6 +8,7 @@
       eshell-history-size 20000
       eshell-save-history-on-exit t
       eshell-hist-ignoredups t
+      eshell-prefer-lisp-functions t
       eshell-scroll-to-bottom-on-output t
       eshell-scroll-show-maximum-output t
       eshell-cmpl-dir-ignore "\\`\\(\\.\\.?\\|CVS\\|\\.svn\\|\\.git\\)/\\'")
@@ -54,5 +55,36 @@
              (eshell-parse-command (car l) (cdr l))))))
 
 (put 'eshell/ec 'eshell-no-numeric-conversions t)
+
+(defun tyler-eshell-view-file (file)
+  "A version of `view-file' which properly respects the eshell prompt."
+  (interactive "fView file: ")
+  (unless (file-exists-p file) (error "%s does not exist" file))
+  (let ((had-a-buf (get-file-buffer file))
+        (buffer (find-file-noselect file)))
+    (if (eq (with-current-buffer buffer (get major-mode 'mode-class))
+            'special)
+        (progn
+          (switch-to-buffer buffer)
+          (message "Not using View mode because the major mode is special"))
+      (let ((undo-window (list (window-buffer) (window-start)
+                               (+ (window-point)
+                                  (length (funcall eshell-prompt-function))))))
+        (switch-to-buffer buffer)
+        (view-mode-enter (cons (selected-window) (cons nil undo-window))
+                         'kill-buffer)))))
+
+(defun eshell/less (&rest args)
+  "Invoke `view-file' on a file. \"less +42 foo\" will go to line 42 in
+    the buffer for foo."
+  (while args
+    (if (string-match "\\`\\+\\([0-9]+\\)\\'" (car args))
+        (let* ((line (string-to-number (match-string 1 (pop args))))
+               (file (pop args)))
+          (tyler-eshell-view-file file)
+          (goto-line line))
+      (tyler-eshell-view-file (pop args)))))
+
+(defalias 'eshell/more 'eshell/less)
 
 (provide 'eshell-conf)
