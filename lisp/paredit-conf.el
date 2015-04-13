5591a90f2053bcf8f809174cc058285b3ed11001
Robert Crim <rob@servermilk.com>
2015-04-12 20:45:00 -0700

    Temp fix since Terminal broke my C-arrows

--- lisp/paredit-conf.el
+++ lisp/paredit-conf.el
@@ -1,3 +1,7 @@
 (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
 
+(eval-after-load "paredit"
+  '(progn (define-key paredit-mode-map (kbd "C-c <right>") 'paredit-forward-slurp-sexp)
+          (define-key paredit-mode-map (kbd "C-c <left>") 'paredit-forward-barf-sexp)))
+
 (provide 'paredit-conf)
