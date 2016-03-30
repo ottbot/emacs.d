17b08d412c042b65fabf60a876f752439686ce69
Robert Crim <rob@servermilk.com>
2016-03-30 13:41:43 -0700

    Add things to PATH

--- lisp/my-env.el
+++ lisp/my-env.el
@@ -1,14 +1,12 @@
+(dolist (p '("/usr/local/bin" "Library/TeX/texbin" "~/.cabal/bin"))
+  (setenv "PATH" (concat (getenv "PATH") ":" p))
+  (setq exec-path (append exec-path '(p))))
+
 (setenv "BOOT_CLOJURE_VERSION" "1.7.0")
 (setenv "BOOT_JVM_OPTIONS" "-client -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xverify:none")
 
-(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
-(setq exec-path (append exec-path '("/usr/local/bin")))
-
-(setenv "PATH" (concat (getenv "PATH") ":/usr/texbin"))
-(setq exec-path (append exec-path '("/usr/texbin")))
-
-
-(setenv "ZOOKEEPER_ADDRESS" "localhost:2181")
+(setenv "KAFKA_ADDRESS" "192.168.99.100:9092")
+(setenv "ZOOKEEPER_ADDRESS" "192.168.99.100:2181")
 (setenv "SCHEMA_REGISTRY_URL" "http://localhost:8081")
 
 (setenv "RIEMANN_HOST" "localhost")
