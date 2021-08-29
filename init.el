af9ab43a13d28d16ae846b3c966e05f453472e79
Robert Crim <rob@servermilk.com>
2021-08-29 08:12:38 -0700

    initial commit

---
+++ init.el
@@ -0,0 +1,163 @@
+;;--- bootstrap straight.el
+
+(defvar bootstrap-version)
+
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+(setq-default  straight-fix-org t
+               straight-fix-flycheck t
+               straight-use-package-by-default t)
+
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+;;-----
+
+
+(setq user-full-name "Robert Crim"
+      user-mail-address "rob@servermilk.com")
+
+;; don't persist customizations
+(setq custom-file (make-temp-file "tmp-emacs-custom"))
+
+;; backups
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+  backup-by-copying t
+  version-control t
+  delete-old-versions t
+  kept-new-versions 20
+  kept-old-versions 5
+  )
+
+;; visual
+
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+(blink-cursor-mode -1)
+
+
+(global-hl-line-mode +1)
+(column-number-mode t)
+(size-indication-mode t)
+(setq inhibit-startup-screen t)
+
+(setq scroll-margin 0
+      scroll-conservatively 100000
+      scroll-preserve-screen-position 1)
+
+(set-frame-font "Dank Mono 15" nil t)
+
+(setq-default indent-tabs-mode nil)
+
+(use-package doom-themes
+  :config
+  (load-theme 'doom-one-light t)
+  (doom-themes-visual-bell-config))
+
+(use-package diminish)
+
+
+
+
+
+(use-package smartparens
+  :ensure t
+  :diminish smartparens-mode
+  :config
+  (progn
+    (require 'smartparens-config)
+    (smartparens-global-mode 1)
+    (show-paren-mode t)))
+
+(use-package crux
+  :bind
+  ("C-k" . crux-smart-kill-line)
+  ("C-c n" . crux-cleanup-buffer-or-region)
+  ("C-c f" . crux-recentf-find-file)
+  ("C-a" . crux-move-beginning-of-line))
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(use-package expand-region
+  :bind ("M-m" . er/expand-region))
+
+
+(use-package feebleline
+  :config
+  (feebleline-mode 't))
+
+(use-package emojify)
+
+;; macos stuff
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . dark))
+
+
+(use-package avy
+  :bind
+  ("C-=" . avy-goto-char)
+  :config
+  (setq avy-background t))
+
+(use-package company
+  :diminish company-mode
+  :config
+  (add-hook 'after-init-hook #'global-company-mode))
+
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)))
+
+(use-package prescient)
+
+(use-package ivy-prescient
+  :config
+  (ivy-prescient-mode t))
+
+
+(use-package ivy-hydra)
+
+(use-package major-mode-hydra
+  :bind
+  ("C-M-SPC" . major-mode-hydra)
+  :config
+  (major-mode-hydra-define org-mode
+    ()
+    ("Tools"
+     (("l" org-lint "lint")))))
+
+(use-package which-key
+  :diminish which-key-mode
+  :config
+  (add-hook 'after-init-hook 'which-key-mode))
+
+(use-package ace-window
+   :config
+   (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)))
+
+(use-package fzf)
+
+(use-package exec-path-from-shell
+  :config
+  (exec-path-from-shell-initialize))
+
+(use-package magit
+  :bind ("C-x g" . magit-status))
+
+(use-package parinfer-rust-mode
+  :hook emacs-lisp-mode
+  :init
+  (setq parinfer-rust-library "~/.emacs.d/parinfer-rust/parinfer-rust-arm64.so"))
+
+(use-package eglot)
+(use-package fsharp-mode)
