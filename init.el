9448f784af6edc6211ce9b4a7feb3b134da5813e
Robert Crim <rob@servermilk.com>
2021-09-01 09:01:11 -0700

    Can't can't opam-user-setup in..

--- init.el
+++ init.el
@@ -59,13 +59,12 @@
 
 (setq-default indent-tabs-mode nil)
 
-
 ;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
 (require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
-(require 'dune)
 ;; ## end of OPAM user-setup addition for emacs / base ## keep this line
 
-
+(fset 'yes-or-no-p 'y-or-n-p)
+(global-auto-revert-mode)
 
 (use-package vterm)
 
