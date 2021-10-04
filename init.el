0861135828f210b3409a848f141300ccc1dbb872
Robert Crim <rob@servermilk.com>
2021-10-03 20:01:44 -0700

    ok

--- init.el
+++ init.el
@@ -2,19 +2,12 @@
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
 (require 'rc-base)
-(require 'rc-fancy)
+(require 'rc-look)
 
 (setq user-full-name "Robert Crim"
       user-mail-address "rob@servermilk.com")
 
-
-(rc/sup '(nano-theme :type git :host github
-                     :repo "rougier/nano-theme"))
-
-(require 'nano-theme)
-
-(nano-dark)
-
+(rc/the-look "JetBrains Mono-13")
 
 (require 'rc-lsp)
 (require 'rc-lisps)
@@ -25,17 +18,5 @@
 ;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
 (require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
 ;; ## end of OPAM user-setup addition for emacs / base ## keep this line
+
 (require 'rc-ocaml)
-
-(defun rc/vterm-is-active ()
-  (string-equal "*vterm*" (buffer-name)))
-
-(defun rc/toggle-vterm ()
-  (interactive)
-  (if (rc/vterm-is-active)
-    (bury-buffer)
-    (vterm)))
-
-(rc/global-key '("s-w" . bury-buffer))
-
-(rc/global-key '("s-t" . rc/toggle-vterm))
--- lisp/rc-base.el
+++ lisp/rc-base.el
@@ -10,29 +10,19 @@
 (setq custom-file "~/.emacs.d/custom.el")
 
 ;; backups
-(setq backup-directory-alist '(("." . "~/.emacs.d/backup")) backup-by-copying t version-control t
-      delete-old-versions t kept-new-versions 20 kept-old-versions 5)
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+      backup-by-copying t
+      version-control t
+      delete-old-versions t
+      kept-new-versions 20
+      kept-old-versions 5)
 
 (setq-default indent-tabs-mode nil)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 (global-auto-revert-mode)
 
-;; macos stuff
-(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-(add-to-list 'default-frame-alist '(ns-appearance . dark))
-
 (recentf-mode 1)
-
-(menu-bar-mode -1)
-(toggle-scroll-bar -1)
-(tool-bar-mode -1)
-(blink-cursor-mode -1)
-(global-hl-line-mode +1)
-(column-number-mode t)
-(size-indication-mode t)
-
-(setq inhibit-startup-screen t)
 
 (setq scroll-margin 0
       scroll-conservatively 100000
@@ -42,17 +32,77 @@
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
+(rc/sup 'fzf)
+(rc/sup 'vterm)
+(rc/sup 'unkillable-scratch)
+
+
+(defun rc/vterm-is-active ()
+  (string-equal "*vterm*" (buffer-name)))
+
+(defun rc/toggle-vterm ()
+  (interactive)
+  (if (rc/vterm-is-active)
+    (bury-buffer)
+    (vterm)))
+
+(rc/global-key '("s-w" . bury-buffer))
+(rc/global-key '("s-t" . rc/toggle-vterm))
+
 
 (progn
   (straight-use-package 'crux)
 
   (let ((crux-keys '(("C-k" . crux-smart-kill-line)
-                     ("C-c n" . cruz-cleanup-buffer-or-region)
                      ("C-c n" . crux-cleanup-buffer-or-region)
                      ("C-c f" . crux-recentf-find-file)
                      ("C-a" . crux-move-beginning-of-line))))
 
     (dolist (kb crux-keys)
       (rc/global-key kb))))
+
+(progn
+  (rc/sup 'magit)
+  (rc/global-key '("C-x g" . magit-status)))
+
+(progn
+ (rc/sup 'expand-region)
+ (rc/global-key '("M-m" . er/expand-region)))
+
+
+(progn
+ (rc/sup 'smartparens)
+ (require 'smartparens-config)
+ (smartparens-global-mode 1)
+ (show-paren-mode t))
+
+
+(progn
+  (rc/sup 'avy)
+  (rc/global-key '("C-=" . avy-goto-char))
+  (setq avy-background t))
+
+
+(progn
+  (dolist (pkg (list 'ivy 'counsel 'swiper 'ivy-hydra 'ivy-prescient))
+    (rc/sup pkg))
+  (ivy-mode 1)
+  (ivy-prescient-mode t)
+  (rc/global-key '("M-x" . counsel-M-x)))
+
+
+(progn
+  (rc/sup 'company)
+  (add-hook 'after-init-hook #'global-company-mode))
+
+
+(progn
+  (rc/sup 'which-key)
+  (add-hook 'after-init-hook 'which-key-mode))
+
+(progn
+  (rc/sup 'ace-window)
+  (rc/global-key '("C-x o" . ace-window)))
+
 
 (provide 'rc-base)
