526f6318c5b5882115b9405438f362d023b2ed20
Robert Crim <rob@servermilk.com>
2021-10-12 07:46:10 -0700

    jet BRAINED

--- lisp/custom.el
+++ lisp/custom.el
@@ -64,6 +64,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(nano-mono ((t (:weight normal :height 140 :family "JetBrains Mono"))))
  '(window-divider ((t (:foreground "gray93")))))
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
