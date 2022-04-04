0c981721902606161bce3e8a77d8e8f6b8963858
Robert Crim <rob@servermilk.com>
2022-04-04 07:16:43 -0700

    lsp + tuareg

--- init.el
+++ init.el
@@ -9,8 +9,22 @@
 (require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
 ;; ## end of OPAM user-setup addition for emacs / base ## keep this line
 
-(use-package tuareg
-  :after (lsp)
-  :hook (tuareg-mode . lsp))
+(use-package tuareg)
+
+(use-package lsp-mode
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :hook ((lsp-mode . lsp-enable-which-key-integration)
+         (tuareg-mode . lsp))
+  :commands lsp)
+
+(use-package lsp-ui
+  :commands lsp-ui-mode)
+
+(use-package lsp-ivy :commands lsp-ivy-workplace-symbol)
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+
+
 
 (load custom-file)
--- lisp/rc.el
+++ lisp/rc.el
@@ -80,19 +80,4 @@
 
 (use-package project)
 
-(use-package lsp-mode
-  :init
-  (setq lsp-keymap-prefix "C-c l")
-  :hook (lsp-mode . lsp-enable-which-key-integration)
-  :commands lsp)
-
-(use-package lsp-ui
-  :commands lsp-ui-mode)
-
-(use-package lsp-ivy :commands lsp-ivy-workplace-symbol)
-(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
-
-
-
-
 (provide 'rc)
